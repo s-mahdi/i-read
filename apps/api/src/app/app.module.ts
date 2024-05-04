1aa31bdee8957fa75a5746199e2d71a4ac2680ec
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-05-05 00:00:06 +0330

    fix typeorm issue

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from '../user/user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from '../config/jwt.config';
+import { User } from '../user/entities/user.entity';
 
 @Module({
   imports: [
@@ -13,11 +16,12 @@ import { UserModule } from '../user/user.module';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      entities: [User],
       synchronize: process.env.TYPEORM_SYNC === 'true', // Controlled by environment variable
       logging: true,
     }),
     UserModule,
+    JwtModule.register(jwtConstants),
   ],
   controllers: [AppController],
   providers: [AppService],
