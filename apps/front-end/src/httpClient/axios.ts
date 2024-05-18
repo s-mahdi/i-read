02fea228f044ff54eb6c77ebc6d070332aca67a0
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-05-18 09:40:30 +0330

    axios interceptor updated

--- apps/front-end/src/httpClient/axios.ts
+++ apps/front-end/src/httpClient/axios.ts
@@ -18,17 +18,4 @@ axiosClient.interceptors.request.use(
   }
 );
 
-axiosClient.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.clear();
-      window.location.href = '/login';
-    }
-    return Promise.reject(error);
-  }
-);
-
 export { axiosClient };
