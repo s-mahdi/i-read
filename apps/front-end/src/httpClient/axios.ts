ecfeeaeca47bcd48bee310324608954b79b4f60d
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-05-27 15:47:11 -0400

    default timeout extended

--- apps/front-end/src/httpClient/axios.ts
+++ apps/front-end/src/httpClient/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 10000,
+  timeout: 50000,
 });
 
 axiosClient.interceptors.request.use(
