f3d9d5460c21b412b672e50e5ad068a2ee5398c9
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-06-10 16:09:11 -0400

    more error handling on auth service

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -1,13 +1,14 @@
-import { ForgetPasswordDto } from './../user/dto/forget-password.dto';
 import {
   Injectable,
   NotFoundException,
   UnauthorizedException,
+  InternalServerErrorException,
 } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
-import { CreateUserDto } from '../user/dto/create-user.dto';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 import { CreateEmployeeDto } from 'src/user/dto/create-employee.dto';
+import { ForgetPasswordDto } from 'src/user/dto/forget-password.dto';
 
 @Injectable()
 export class AuthService {
@@ -17,14 +18,38 @@ export class AuthService {
   ) {}
 
   async login(username: string, pass: string): Promise<any> {
-    const user = await this.userService.findOneByUserName(username);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException('نام کاربری یا کلمه عبور صحیح نیست');
+    try {
+      const user = await this.userService.findOneByUserName(username);
+
+      if (!user) {
+        throw new NotFoundException(
+          'کاربر با این نام کاربری یافت نشد. لطفا ثبت‌نام کنید',
+        );
+      }
+
+      if (user.password !== pass) {
+        throw new UnauthorizedException('نام کاربری یا کلمه عبور صحیح نیست');
+      }
+
+      const payload = {
+        username: user.username,
+        sub: user.id,
+        role: user.role,
+      };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    } catch (error) {
+      if (
+        error instanceof NotFoundException ||
+        error instanceof UnauthorizedException
+      ) {
+        throw error;
+      }
+      throw new InternalServerErrorException(
+        'مشکلی در سرور رخ داده است. لطفاً بعداً دوباره تلاش کنید.',
+      );
     }
-    const payload = { username: user.username, sub: user.id, role: user.role };
-    return {
-      access_token: this.jwtService.sign(payload),
-    };
   }
 
   async signup(signUpDto: CreateUserDto): Promise<any> {
