610d3562535d81c1f06e67cc58711b61d31194a4
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-06-16 01:36:44 -0400

    env updated

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.HOST_ADDRESS],
+    origin: [
+      process.env.HOST_ADDRESS,
+      process.env.HOST_ADDRESS_2,
+      process.env.HOST_ADDRESS_3,
+    ],
   });
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
