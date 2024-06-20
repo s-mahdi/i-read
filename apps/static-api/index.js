91946fe47b023b137747a350de7571b0cbdae58f
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-06-20 03:20:25 -0400

    cors fixed on static api

--- apps/static-api/index.js
+++ apps/static-api/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const audioPath = path.join(__dirname, 'assets', 'audio')
+
+const allowedOrigins = process.env.ALLOWED_ORIGINS ? process.env.ALLOWED_ORIGINS.split(',') : '*';
+const corsOptions = {
+  origin: allowedOrigins
+};
+
+app.use(cors(corsOptions));
 
 app.use('/audio', express.static(audioPath));
 
