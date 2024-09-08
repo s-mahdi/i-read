44dc85693100fb5d53126d3cc78273f00f1da2b4
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-09-08 11:12:40 -0500

    unit api set to public

--- apps/api/src/units/units.controller.ts
+++ apps/api/src/units/units.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UnitsService } from './units.service';
 import { Unit } from './entities/unit.entity';
+import { Public } from '../decorators/public.decorator';
 
 @Controller('units')
 export class UnitsController {
   constructor(private readonly unitsService: UnitsService) {}
 
+  @Public()
   @Get()
   findAll(): Promise<Unit[]> {
     return this.unitsService.findAll();
