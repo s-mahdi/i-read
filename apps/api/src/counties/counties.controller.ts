14ff9b426045ca0ce30083aa75f568af77962524
Mahdi Hoseini <mahdi.hoseini.dev@gmail.com>
2024-09-08 10:26:50 -0500

    county endpoint set to public

--- apps/api/src/counties/counties.controller.ts
+++ apps/api/src/counties/counties.controller.ts
@@ -1,21 +1,25 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { CountiesService } from './counties.service';
 import { County } from './entities/county.entity';
+import { Public } from '../decorators/public.decorator';
 
 @Controller('counties')
 export class CountiesController {
   constructor(private readonly countiesService: CountiesService) {}
 
+  @Public()
   @Get()
   findAll(): Promise<County[]> {
     return this.countiesService.findAll();
   }
 
+  @Public()
   @Get(':id')
   findOne(@Param('id') id: number): Promise<County> {
     return this.countiesService.findOne(id);
   }
 
+  @Public()
   @Get('province/:provinceId')
   findByProvince(@Param('provinceId') provinceId: number): Promise<County[]> {
     return this.countiesService.findByProvince(provinceId);
